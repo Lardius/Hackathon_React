6fddadedbf8da52a560cc91b43a961cb97598f84
Vlad Muravyov <vlad.muravyov@mail.ru>
2021-10-10 21:27:21 +0500

    refact contrastColor.js

--- src/utils/contrastColor.js
+++ src/utils/contrastColor.js
@@ -1,7 +1,7 @@
 export const getContrastColor = (hexcolor) => {
-  var r = parseInt(hexcolor.substr(1, 2), 16)
-  var g = parseInt(hexcolor.substr(3, 2), 16)
-  var b = parseInt(hexcolor.substr(5, 2), 16)
-  var yiq = (r * 299 + g * 587 + b * 114) / 1000
-  return yiq >= 128 ? 'black' : 'white'
+  const r = parseInt(hexcolor.substr(1, 2), 16)
+  const g = parseInt(hexcolor.substr(3, 2), 16)
+  const b = parseInt(hexcolor.substr(5, 2), 16)
+  const rgb = (r * 299 + g * 587 + b * 114) / 1000
+  return rgb >= 128 ? 'black' : 'white'
 }
